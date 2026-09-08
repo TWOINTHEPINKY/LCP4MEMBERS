a0d36395eb3e829d531819d60fe0096294a2de37
Jesse <coldnoct@gmail.com>
2026-09-08 23:28:14 +0400

    conf

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lcp4members/', // ← ВАЖНО: слэши в начале и конце!
+  base: '/LCP4MEMBERS/', 
 })
